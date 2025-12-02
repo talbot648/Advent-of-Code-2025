32b931274c07e5dbe69d76ab6e648fa3eeba9c67
talbot648 <charlietalbot648@gmail.com>
2025-12-02 16:26:40 +0000

    added text file reader

--- dayTwo/dayTwo.go
+++ dayTwo/dayTwo.go
@@ -1,10 +1,37 @@
 package dayTwo
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 )
 
 func Solution() int {
-	fmt.Println("Day Two Solution")
+	input, err := readInput("dayTwo/inputDayTwo.txt")
+	if err != nil {
+		fmt.Println("error reading file", err)
+		return 0
+	}
+	fmt.Println(input)
 	return 0
+}
+
+func readInput(filename string) (string, error) {
+	file, err := os.Open(filename)
+	if err != nil {
+		return "", err
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	if !scanner.Scan() {
+		if err := scanner.Err(); err != nil {
+			return "", err
+		}
+		// No data in the file
+		return "", fmt.Errorf("file is empty")
+	}
+
+	return scanner.Text(), nil
 }
--- main.go
+++ main.go
@@ -1,16 +1,15 @@
 package main
 
 import (
-	dayOne "AdventOfCode/dayOne"
 	"AdventOfCode/dayTwo"
 	"fmt"
 )
 
 func main() {
-	dayOneAnswer := dayOne.Solution()
-	fmt.Println(dayOneAnswer)
-	dayOnePartTwoAnswer := dayOne.SolutionPTwo()
-	fmt.Println(dayOnePartTwoAnswer)
+	// dayOneAnswer := dayOne.Solution()
+	// fmt.Println(dayOneAnswer)
+	// dayOnePartTwoAnswer := dayOne.SolutionPTwo()
+	// fmt.Println(dayOnePartTwoAnswer)
 	dayTwoAnswer := dayTwo.Solution()
 	fmt.Println(dayTwoAnswer)
 }
